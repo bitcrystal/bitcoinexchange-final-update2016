f64c1ed649517445b86be09a269f24f901e1ee02
bitcrystal <bitcrystalv30@gmail.com>
2016-02-22 04:29:58 +0100

    Delete database.php

--- database.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbdb_host = 'localhost';         // the server the database is located
-$dbdb_user = 'bitcoinexchange';      // the username to access the database
-$dbdb_pass = 'xxxxxxxxxxxxxxxxxxxxx';      // the password to access the database
-$dbdb_database = 'bitcoinexchange';  // the name of the database being used
-?>
